ce6cc40ff94216b9d314d3a6955e691c5c72e3b6
Myron819 <19036849@rp.edu.sg>
2021-07-29 14:22:24 +0800

    Updated

--- app/src/main/java/sg/edu/rp/c346/id19004781/c347_ps11_taskmanager/AddActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19004781/c347_ps11_taskmanager/AddActivity.java
@@ -1,7 +1,14 @@
 package sg.edu.rp.c346.id19004781.c347_ps11_taskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 public class AddActivity extends AppCompatActivity {
@@ -10,5 +17,46 @@ public class AddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        // Temporary
+        setNotification(5);
+    }
+
+    private void setNotification(int seconds) {
+        int notificationID = 888;
+        int requestCode = 123;
+
+        NotificationManager notificationManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new
+                    NotificationChannel("default", "Default Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            channel.setDescription("This is for default notification");
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        Intent intent = new Intent(AddActivity.this,AddActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity
+                ( AddActivity.this, requestCode, intent,
+                        PendingIntent.FLAG_CANCEL_CURRENT);
+
+        // Build notification
+        NotificationCompat.Builder builder = new
+                NotificationCompat.Builder(AddActivity.this, "default");
+        builder.setContentTitle("Amazing Offer!");
+        builder.setContentText("Subject");
+        builder.setSmallIcon(android.R.drawable.btn_star_big_off);
+        builder.setContentIntent(pIntent);
+        builder.setAutoCancel(true);
+
+        Notification n = builder.build();
+
+        // An integer good to have, for you to programmatically cancel it
+        notificationManager.notify(notificationID, n);
+        finish();
+
     }
 }
