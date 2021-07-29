2d833b46d11151b016db5f7107764d51dd3515c8
Myron819 <19036849@rp.edu.sg>
2021-07-29 15:50:12 +0800

    Updated

--- app/src/main/java/sg/edu/rp/c346/id19004781/c347_ps11_taskmanager/AddActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19004781/c347_ps11_taskmanager/AddActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.id19004781.c347_ps11_taskmanager;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -10,6 +11,8 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+
+import java.util.Calendar;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -19,52 +22,79 @@ public class AddActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add);
 
         // Temporary
-        setNotification("Task Manager Reminder", "Post Letters", 5);
+        setNotification("this is the name!", "description!!", 5);
     }
 
     private void setNotification(String name, String description, int seconds) {
         int notificationID = 888;
         int requestCode = 123;
 
-        NotificationManager notificationManager = (NotificationManager)
-                getSystemService(NOTIFICATION_SERVICE);
+//        NotificationManager notificationManager = (NotificationManager)
+//                getSystemService(NOTIFICATION_SERVICE);
+//
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            NotificationChannel channel = new
+//                    NotificationChannel("default", "Default Channel",
+//                    NotificationManager.IMPORTANCE_DEFAULT);
+//
+//            channel.setDescription("This is for default notification");
+//            notificationManager.createNotificationChannel(channel);
+//        }
+//
+//        Intent intent = new Intent(AddActivity.this,AddActivity.class);
+//        PendingIntent pIntent = PendingIntent.getActivity
+//                ( AddActivity.this, requestCode, intent,
+//                        PendingIntent.FLAG_CANCEL_CURRENT);
+//
+//        // Bigtext
+//        NotificationCompat.BigTextStyle bigText = new
+//                NotificationCompat.BigTextStyle();
+//        bigText.setBigContentTitle("Big Text – Long Content");
+//        bigText.bigText("This is one big text" +
+//                " - A quick brown fox jumps over a lazy brown dog "+
+//                "\nLorem ipsum dolor sit amet, sea eu quod des");
+//        bigText.setSummaryText("Reflection Journal?");
+//
+//        // Build notification
+//        NotificationCompat.Builder builder = new
+//                NotificationCompat.Builder(AddActivity.this, "default");
+//        builder.setContentTitle(name);
+//        builder.setContentText(description);
+//        builder.setSmallIcon(android.R.drawable.btn_star_big_off);
+//        builder.setContentIntent(pIntent);
+//        builder.setStyle(bigText); // Bigtext
+//        builder.setAutoCancel(true);
+//
+//        Notification n = builder.build();
+//
+//        // An integer good to have, for you to programmatically cancel it
+//        notificationManager.notify(notificationID, n);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new
-                    NotificationChannel("default", "Default Channel",
-                    NotificationManager.IMPORTANCE_DEFAULT);
 
-            channel.setDescription("This is for default notification");
-            notificationManager.createNotificationChannel(channel);
-        }
 
-        Intent intent = new Intent(AddActivity.this,AddActivity.class);
-        PendingIntent pIntent = PendingIntent.getActivity
-                ( AddActivity.this, requestCode, intent,
-                        PendingIntent.FLAG_CANCEL_CURRENT);
 
-        NotificationCompat.BigTextStyle bigText = new
-                NotificationCompat.BigTextStyle();
-        bigText.setBigContentTitle("Big Text – Long Content");
-        bigText.bigText("This is one big text" +
-                " - A quick brown fox jumps over a lazy brown dog "+
-                "\nLorem ipsum dolor sit amet, sea eu quod des");
-        bigText.setSummaryText("Reflection Journal?");
+// ---
 
-        // Build notification
-        NotificationCompat.Builder builder = new
-                NotificationCompat.Builder(AddActivity.this, "default");
-        builder.setContentTitle(name);
-        builder.setContentText(description);
-        builder.setSmallIcon(android.R.drawable.btn_star_big_off);
-        builder.setContentIntent(pIntent);
-        builder.setStyle(bigText);
-        builder.setAutoCancel(true);
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.SECOND, seconds);
 
-        Notification n = builder.build();
+        Intent intent1 = new Intent(AddActivity.this,
+                ScheduledNotificationReceiver.class);
 
-        // An integer good to have, for you to programmatically cancel it
-        notificationManager.notify(notificationID, n);
+        intent1.putExtra("name", name);
+        intent1.putExtra("description", description);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                AddActivity.this, requestCode,
+                intent1, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        AlarmManager am = (AlarmManager)
+                getSystemService(AddActivity.ALARM_SERVICE);
+        am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
+                pendingIntent);
+
+
+
         finish();
 
     }
---
+++ app/src/main/java/sg/edu/rp/c346/id19004781/c347_ps11_taskmanager/ScheduledNotificationReceiver.java
@@ -0,0 +1,59 @@
+package sg.edu.rp.c346.id19004781.c347_ps11_taskmanager;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+
+public class ScheduledNotificationReceiver extends BroadcastReceiver {
+
+    int reqCode = 12345;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String name = intent.getExtras().getString("name");
+        String description = intent.getExtras().getString("description");
+
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new
+                    NotificationChannel("default", "Default Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            channel.setDescription("This is for default notification");
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        Intent i = new Intent(context, MainActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(context, reqCode,
+                i, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        // Bigtext
+        NotificationCompat.BigTextStyle bigText = new
+                NotificationCompat.BigTextStyle();
+        bigText.setBigContentTitle(name + " (bigtext)");
+        bigText.bigText(description + " (bigtext)\n");
+
+        // build notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default");
+        builder.setContentTitle(name);
+        builder.setContentText(description);
+        builder.setSmallIcon(android.R.drawable.ic_dialog_info);
+        builder.setContentIntent(pIntent);
+        builder.setStyle(bigText); // Bigtext
+        builder.setAutoCancel(true);
+
+        Notification n = builder.build();
+        notificationManager.notify(123, n);
+
+
+    }
+}
