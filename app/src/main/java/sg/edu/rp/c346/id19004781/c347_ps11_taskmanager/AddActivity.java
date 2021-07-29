ac06996a4e808817f6d9dd97df0240281cdd1824
zuhaili <19004781@myrp.edu.sg>
2021-07-29 13:08:37 +0800

    Created Second Activity and XML named AddActivity, activity_add

---
+++ app/src/main/java/sg/edu/rp/c346/id19004781/c347_ps11_taskmanager/AddActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id19004781.c347_ps11_taskmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
--- app/src/main/java/sg/edu/rp/c346/id19004781/c347_ps11_taskmanager/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19004781/c347_ps11_taskmanager/MainActivity.java
@@ -2,13 +2,31 @@ package sg.edu.rp.c346.id19004781.c347_ps11_taskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lvTasks;
+    Button btnAddTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lvTasks = findViewById(R.id.lvTasks);
+        btnAddTask = findViewById(R.id.btnAddTask);
+
+        btnAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,AddActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
