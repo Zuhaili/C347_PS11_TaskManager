bb0772ae44e7ccfa2ffd9bf974a6389839c7ef6f
LI SHUFANG <50829212+Cr0ckyy@users.noreply.github.com>
2021-07-29 23:06:09 +0800

    UPDATED

--- app/src/main/java/sg/edu/rp/c346/id19004781/c347_ps11_taskmanager/ScheduledNotificationReceiver.java
+++ app/src/main/java/sg/edu/rp/c346/id19004781/c347_ps11_taskmanager/ScheduledNotificationReceiver.java
@@ -75,8 +75,8 @@ public class ScheduledNotificationReceiver extends BroadcastReceiver {
                 "Oh-whoo");
 
 
-        builder.setContentTitle("Bad Day (I Feel Good song) Lyrics");
-        builder.setContentText("Birds flying high, you know how I feel");
+        builder.setContentTitle("James Brown(I Feel Good song) Lyrics");
+        builder.setContentText("James Joseph Brown (May 3, 1933 – December 25, 2006) was an American singer, songwriter, dancer, musician, record producer, and bandleader. The central progenitor of funk music and a major figure of 20th-century music, he is often referred to by the honorific nicknames \"Godfather of Soul\", \"Mr. Dynamite\", and \"Soul Brother No. 1\".In a career that lasted over 50 years, he influenced the development of several music genres.");
         builder.setSmallIcon(android.R.drawable.ic_dialog_info);
         builder.setContentIntent(pIntent);
         builder.setStyle(bigText);
